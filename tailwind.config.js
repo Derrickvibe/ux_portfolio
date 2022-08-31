fd22ac2cd74bfccf59918f93038c39ff78bf2391
Xander Selorm <xanderselorm@gmail.com>
2022-08-31 00:00:14 +0000

    finished setting up structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}", "./pages/**/*.{html,js}"],
   theme: {
     colors: {
       black: '#292A2C',
